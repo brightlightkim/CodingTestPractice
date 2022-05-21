79f1fd23423e48e9814de2841a7c211a47809c9e
brightlightkim <taeyangk95@naver.com>
2022-05-21 12:46:49 -0600

    ThreeStackInOneArray

---
+++ src/DataStructures/Stacks/ThreeStackInOne.java
@@ -0,0 +1,10 @@
+package DataStructures.Stacks;
+
+public class ThreeStackInOne {
+    private int numberOfStacks = 3;
+    private int stackCapacity;
+    private int[] values;
+    private int[] sizes;
+
+    public ThreeStackInOne
+}
